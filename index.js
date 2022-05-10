28a1419e801041c62cd895949816d38041e5ba71
Sheikh Tafsir Rahman <83116065+Sheikh-Tafsir@users.noreply.github.com>
2022-05-10 17:27:57 +0600

    dual theme added

--- index.js
+++ index.js
@@ -10,3 +10,58 @@ function settmin(){
     document.querySelector(".xmark").style.visibility = "hidden";
     document.querySelector(".bars").style.visibility = "visible";
 }
+function settlight(){
+    document.querySelector("body").classList.add("body-light");
+    document.querySelector(".header").classList.add("header-light");
+    document.querySelector(".headmenu").classList.add("headmenu-light");
+    document.querySelector(".banner").classList.add("banner-light");
+    document.querySelector(".motto").classList.add("motto-light");
+    document.querySelector(".place-bar").classList.add("place-bar-light");
+    document.querySelector(".ab").classList.add("ab-light");
+    document.querySelector(".ot").classList.add("ot-light");
+    document.querySelector(".con").classList.add("con-light");
+    document.querySelector(".footer").classList.add("footer-light");
+    document.querySelector('.footer').getElementsByTagName('h1')[0].style.color="black";
+ 
+    if (window.innerWidth>=0 && window.innerWidth<992){
+        var ainside = document.querySelector('.headmenu').getElementsByTagName('a');
+        for(i=0;i<ainside.length;i++) {
+            ainside[i].style.color="black";
+        }
+        ainside = document.querySelector('.headmenu').getElementsByTagName('i');
+        for(i=0;i<ainside.length;i++) {
+            ainside[i].style.color="black";
+        }
+    }
+    
+    document.querySelector(".dark").style.visibility = "visible";
+    document.querySelector(".light").style.visibility = "hidden";
+
+}
+function settdark(){
+    document.querySelector("body").classList.remove("body-light");
+    document.querySelector(".header").classList.remove("header-light");
+    document.querySelector(".headmenu").classList.remove("headmenu-light");
+    document.querySelector(".banner").classList.remove("banner-light");
+    document.querySelector(".motto").classList.remove("motto-light");
+    document.querySelector(".place-bar").classList.remove("place-bar-light");
+    document.querySelector(".ab").classList.remove("ab-light");
+    document.querySelector(".ot").classList.remove("ot-light");
+    document.querySelector(".con").classList.remove("con-light");
+    document.querySelector(".footer").classList.remove("footer-light");
+    document.querySelector('.footer').getElementsByTagName('h1')[0].style.color="white";
+
+    if (window.innerWidth>=0 && window.innerWidth<992){
+        var ainside = document.querySelector('.headmenu').getElementsByTagName('a');
+        for(i=0;i<ainside.length;i++) {
+            ainside[i].style.color="white";
+        }
+        ainside = document.querySelector('.headmenu').getElementsByTagName('i');
+        for(i=0;i<ainside.length;i++) {
+            ainside[i].style.color="white";
+        }
+    }
+
+    document.querySelector(".dark").style.visibility = "hidden";
+    document.querySelector(".light").style.visibility = "visible";
+}
